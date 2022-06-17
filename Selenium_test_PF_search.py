e35b4ff181d0edf73c4ad3c7128b00369153e345
DmitShu <dmitshu@gmail.com>
2022-06-17 20:28:56 +0300

    petfr test 2

--- Selenium_test_PF_search.py
+++ Selenium_test_PF_search.py
@@ -4,7 +4,7 @@ from logins import *
 
 @pytest.fixture(autouse=True)
 def testing():
-   pytest.driver = webdriver.Chrome('driver/chromedriver.exe')
+   pytest.driver = webdriver.Firefox()
    # Переходим на страницу авторизации
    pytest.driver.get(url+'login')
 
@@ -22,3 +22,15 @@ def test_show_my_pets():
    pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
    # Проверяем, что мы оказались на главной странице пользователя
    assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
+
+
+# def test_show_my_pets(selenium):
+#    selenium.get(url+'login')
+#    # Вводим email
+#    selenium.find_element_by_id('email').send_keys(valid_email)
+#    # Вводим пароль
+#    selenium.find_element_by_id('pass').send_keys(valid_password)
+#    # Нажимаем на кнопку входа в аккаунт
+#    selenium.find_element_by_css_selector('button[type="submit"]').click()
+#    # Проверяем, что мы оказались на главной странице пользователя
+#    assert selenium.find_element_by_tag_name('h1').text == "PetFriends"
